4e4a25d2cd1ead77ac10466496d8484e52d5d895
Abhijeet Poudel <abhijeet.poudel@grepsr.com>
2026-01-24 14:43:39 +0545

    DAP config

--- .config/nvim/lua/daze/plugins.lua
+++ .config/nvim/lua/daze/plugins.lua
@@ -206,32 +206,6 @@ return {
 	{
 		"mxsdev/nvim-dap-vscode-js",
 		dependencies = { "mfussenegger/nvim-dap" },
-		config = function()
-			require("dap-vscode-js").setup({
-				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
-			})
-
-			for _, language in ipairs({ "typescript", "javascript" }) do
-				require("dap").configurations[language] = {
-					{
-						{
-							type = "pwa-node",
-							request = "launch",
-							name = "Launch file",
-							program = "${file}",
-							cwd = "${workspaceFolder}",
-						},
-						{
-							type = "pwa-node",
-							request = "attach",
-							name = "Attach",
-							processId = require("dap.utils").pick_process,
-							cwd = "${workspaceFolder}",
-						},
-					},
-				}
-			end
-		end,
 	},
 	{
 		"AndrewRadev/tagalong.vim",
